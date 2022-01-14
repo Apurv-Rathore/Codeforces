3245b6838a6520fff6d3a1526babea2ce00fb6b2
Apurv Rathore <thelegend2710@gmail.com>
2022-01-14 23:39:35 +0530

    Add solution for 1450A

---
+++ 1450/A.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+#include<vector>
+//#define ll long long int  
+#define endl "\n"
+using namespace std;
+
+int main()
+{
+    
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int n;
+        cin>>n;
+        string s;
+        cin>>s;
+        string ans = "";
+        for (auto i:s)
+        {
+            if (i=='b')
+            {
+                ans = 'b' + ans;
+            }
+            else
+            {
+                ans+=i;
+            }
+        }
+        cout<<ans<<endl;
+    
+        
+    }
+}
