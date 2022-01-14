fe11f64eed2e3b35972e0ddc1f0c1d1a2b466ab3
Apurv Rathore <thelegend2710@gmail.com>
2022-01-14 23:41:36 +0530

    Add solution for 1348B

---
+++ 1348/B.py
@@ -0,0 +1,25 @@
+t=int(input())
+while(t):
+    t-=1
+    n,k = [int(i) for i in input().split()]
+    a = [int(i) for i in input().split()]
+    aset = set(a)
+    if len(aset)>k:
+        print(-1)
+    else:
+        ans = []
+        count = [0]*(n+1)
+        for i in aset:
+            count[i]=1
+        alst = list(aset)
+        if len(alst)<k:
+            for i in range (1,n+1):
+                if count[i]==0:
+                    alst.append(i)
+                if len(alst)==k:
+                    break
+        alst.sort()
+        for i in range (n):
+            ans.extend(alst)
+        print(len(ans))
+        print(*ans)
