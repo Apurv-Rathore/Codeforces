f5901223aaa3d26d25ee1d5fe907a19398fb8057
Apurv Rathore <thelegend2710@gmail.com>
2022-01-14 22:53:06 +0530

    Add solution for 1624A

---
+++ 1624/A.py
@@ -0,0 +1,29 @@
+import sys,os,io
+import math 
+from collections import defaultdict
+def ii():
+    return int(input())
+def li():
+    return list(map(int,input().split()))
+if(os.path.exists('input.txt')):
+    sys.stdin = open("input.txt","r") ; sys.stdout = open("output.txt","w") 
+else:
+    input = io.BytesIO(os.read(0, os.fstat(0).st_size)).readline
+
+def solve():
+    n = ii()
+    a = li()
+    print(max(a)-min(a))
+    
+             
+
+
+
+        
+
+
+
+t = 1
+t = ii()
+for _ in range(t):
+    solve()
