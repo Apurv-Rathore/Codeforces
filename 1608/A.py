5d90bb5699db2c07a7adfa98c330d1d2cf7e6eeb
Apurv Rathore <thelegend2710@gmail.com>
2022-01-14 22:54:02 +0530

    Add solution for 1608A

---
+++ 1608/A.py
@@ -0,0 +1,21 @@
+import sys,os,io
+import math 
+from collections import defaultdict
+def ii():
+    return int(input())
+def li():
+    return list(map(int,input().split()))
+if(os.path.exists('input.txt')):
+    sys.stdin = open("input.txt","r") ; sys.stdout = open("output.txt","w") 
+else:
+    input = io.BytesIO(os.read(0, os.fstat(0).st_size)).readline
+
+def solve():
+    n = ii()
+    l = [i for i in range(2,n+2)]
+    print(*l)
+
+t = 1
+t = ii()
+for _ in range(t):
+    solve()
