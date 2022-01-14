ce55771b700c213f1d2fcbc81016b0b3fdd87aa7
Apurv Rathore <thelegend2710@gmail.com>
2022-01-14 23:17:20 +0530

    Add solution for 1389A

---
+++ 1389/A.py
@@ -0,0 +1,9 @@
+from collections import defaultdict
+t = int(input())
+for _ in range(t):
+    
+    l,r = list(map(int,input().split()))
+    if 2*l<=r:
+        print(l,2*l)
+    else:
+        print(-1,-1)
