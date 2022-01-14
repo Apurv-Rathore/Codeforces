22fdc7465a05faa0973165ec8f739b260098e8a2
Apurv Rathore <thelegend2710@gmail.com>
2022-01-14 23:17:31 +0530

    Add solution for 1382A

---
+++ 1382/A.py
@@ -0,0 +1,20 @@
+from collections import defaultdict
+t = int(input())
+for _ in range(t):
+#    n = int(input())
+    n,m = map(int,input().split())
+    a = list(map(int,input().split()))
+    b = list(map(int,input().split()))
+    dic_a = defaultdict(lambda :0)
+    for i in a:
+        dic_a[i]+=1
+    tmp=0
+    for i in b:
+        if dic_a[i]!=0:
+            print("YES")
+            print(1,i)
+            tmp=1
+            break
+    if tmp==0:
+        print("NO")
+   
