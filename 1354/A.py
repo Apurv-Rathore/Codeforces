2a77a6161f9bf9b4d3b45cabfbe7532fa000f1eb
Apurv Rathore <thelegend2710@gmail.com>
2022-01-14 22:42:17 +0530

    Add solution for 1354A

---
+++ 1354/A.py
@@ -0,0 +1,18 @@
+t = int(input())
+for _ in range(t):
+    a,b,c,d = [int(x) for x in input().split()]
+    if a<=b:
+        print(b)
+    elif a>b and d>=c:
+        print(-1)
+    else:
+        x = a-b
+        y = c-d
+        if x%y==0:
+            ans = (x//y)
+        else:
+            ans = ((x//y) +1)
+        print(b + c*ans)
+            
+        
+            
