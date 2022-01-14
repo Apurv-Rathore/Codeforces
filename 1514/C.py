3eef5605f3fbdff634163504789448b2415ed034
Apurv Rathore <thelegend2710@gmail.com>
2022-01-14 22:56:41 +0530

    Add solution for 1514C

---
+++ 1514/C.py
@@ -0,0 +1,104 @@
+#########################################################################################################\
+#########################################################################################################
+###################################The_Apurv_Rathore#####################################################
+#########################################################################################################
+#########################################################################################################
+import sys,os,io
+from sys import stdin
+from math import log, gcd, ceil
+from collections import defaultdict, deque, Counter
+from heapq import heappush, heappop
+from bisect import bisect_left , bisect_right
+import math 
+
+
+alphabets = list('abcdefghijklmnopqrstuvwxyz')
+
+
+def isPrime(x):
+    for i in range(2,x):
+        if i*i>x:
+            break
+        if (x%i==0):
+            return False
+    return True
+def ncr(n, r, p):  
+    num = den = 1
+    for i in range(r):
+        num = (num * (n - i)) % p
+        den = (den * (i + 1)) % p
+    return (num * pow(den,
+                      p - 2, p)) % p
+def primeFactors(n): 
+    l = []
+    while n % 2 == 0: 
+        l.append(2)
+        n = n / 2
+    for i in range(3,int(math.sqrt(n))+1,2): 
+        while n % i== 0: 
+            l.append(int(i))
+            n = n / i 
+    if n > 2: 
+        l.append(n)
+    return list(set(l))
+def power(x, y, p) : 
+	res = 1
+	x = x % p 
+	if (x == 0) : 
+		return 0
+	while (y > 0) : 
+		if ((y & 1) == 1) : 
+			res = (res * x) % p 
+		y = y >> 1	 # y = y/2 
+		x = (x * x) % p 		
+	return res 
+def SieveOfEratosthenes(n):
+    prime = [True for i in range(n+1)]
+    p = 2
+    while (p * p <= n):
+        if (prime[p] == True):
+            for i in range(p * p, n+1, p):
+                prime[i] = False
+        p += 1
+    return prime
+def si():
+    return input()
+def divideCeil(n,x):
+    if (n%x==0):
+        return n//x
+    return n//x+1
+def ii():
+    return int(input())
+def li():
+    return list(map(int,input().split()))
+
+#__________________________TEMPLATE__________________OVER_______________________________________________________
+
+
+if(os.path.exists('input.txt')):
+    sys.stdin = open("input.txt","r") ; sys.stdout = open("output.txt","w") 
+else:
+    input = io.BytesIO(os.read(0, os.fstat(0).st_size)).readline
+
+def solve():
+    n = ii()
+    l = [1]
+    ans=1
+    for i in range(2,n-1):
+        if gcd(n,i)==1:
+            l.append(i)
+            ans=ans*i 
+            ans=ans%n
+    if (n-1 not in l) and (ans*(n-1))%n==1:
+        l.append(n-1)
+    print(len(l))
+    print(*l)
+
+
+
+t = 1
+# t = int(input())
+for _ in range(t):
+    solve()
+    
+
