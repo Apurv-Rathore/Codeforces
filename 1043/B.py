1f704a92f52aec6ebb707bd49fd3ab7ea3ac2388
Apurv Rathore <thelegend2710@gmail.com>
2022-01-14 22:55:12 +0530

    Add solution for 1043B

---
+++ 1043/B.py
@@ -0,0 +1,16 @@
+n=int(input())
+x=list(map(int,input().split()))
+y=[x[0]]
+i=1
+while i<n:
+    y+=[x[i]-x[i-1]]
+    i+=1
+i=1
+z=[]
+while i<=1000 and i<=n:
+    if y==y[:i]*(n//i)+y[:(n%i)]:
+        z+=[i]
+    i+=1
+print (len(z))
+for i in z:
+    print (i,end=' ')
