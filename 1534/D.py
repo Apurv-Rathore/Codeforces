1ab347d2fcde356038886c3e60e4cf7f9d0d7279
Apurv Rathore <thelegend2710@gmail.com>
2022-01-14 23:02:33 +0530

    Add solution for 1534D

---
+++ 1534/D.py
@@ -0,0 +1,29 @@
+e = set()
+def ae(i,j):
+    e.add((min(i,j),max(i,j)))
+ 
+def q(c):
+    print('?',c+1,flush=1)
+    d = [int(i) for i in input().split()]
+    for i in range (n):
+        if d[i]==1:
+            ae(c+1,i+1)
+    return d
+            
+n = int(input())
+d = q(0)
+ev = []
+od = []
+for i in range (n):
+    if d[i]%2:
+        od.append(i)
+    elif d[i]!=0:
+        ev.append(i)
+f=od
+if len(ev)<len(od):
+    f=ev
+for i in f:
+    q(i)
+print('!')
+for i in e:
+    print(*i,flush=1)
