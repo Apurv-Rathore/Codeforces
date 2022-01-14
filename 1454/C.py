2685bfcff8e9a12b3f2daac3abbed8e254aea126
Apurv Rathore <thelegend2710@gmail.com>
2022-01-14 23:39:42 +0530

    Add solution for 1454C

---
+++ 1454/C.py
@@ -0,0 +1,30 @@
+import math
+import sys
+from sys import stdin
+from collections import defaultdict
+t=int(input())
+for _ in range(t):
+    n = int(input())
+    l = [int(x) for x in input().split()]
+    answer = defaultdict(lambda:0)
+    if(len(set(l))==1):
+        print(0)
+        continue
+    prev = [-1]*(n+1)
+    for i in range(n):
+        p = prev[l[i]]
+        if (p==i-1):
+            prev[l[i]] = i
+            continue 
+        answer[l[i]]+=1
+        prev[l[i]]=i
+    m = 100000000000000
+    for i in list(set(l)):
+        if (i==l[-1]):
+            continue
+        answer[i]+=1
+    for i in answer.keys():
+        m = min(m,answer[i])
+    print(m)
+    
+    
