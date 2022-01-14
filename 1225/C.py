1be903895eab2af9e7a2fc8c53bd9701d06d8662
Apurv Rathore <thelegend2710@gmail.com>
2022-01-14 23:38:15 +0530

    Add solution for 1225C

---
+++ 1225/C.py
@@ -0,0 +1,19 @@
+n,p = [int(x) for x in input().split()]
+from math import log 
+f = 0
+def check(x,i):
+    t = list(bin(x))[2:]
+    if (t.count('1')<=i):
+        return True
+    return False
+    
+i  = 0
+for i in range(0,100):
+    
+    if check((n-i*p),i) and n-i*p>=i:
+        print(i)
+        f = 1
+        break
+    
+if (f==0):
+    print(-1)
