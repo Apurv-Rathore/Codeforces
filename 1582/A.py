99c2506c0cfc0f060534a7f4e8b903ea57b874de
Apurv Rathore <thelegend2710@gmail.com>
2022-01-14 22:32:49 +0530

    Add solution for 1582A

---
+++ 1582/A.py
@@ -0,0 +1,26 @@
+import sys,os,io
+import math 
+from collections import defaultdict
+def ii():
+    return int(input())
+def li():
+    return list(map(int,input().split()))
+if(os.path.exists('input.txt')):
+    sys.stdin = open("input.txt","r") ; sys.stdout = open("output.txt","w") 
+else:
+    input = io.BytesIO(os.read(0, os.fstat(0).st_size)).readline
+
+def solve():
+    a,b,c = li()
+    x = a + 2*b + 3*c 
+    print(x%2)
+
+t = 1
+t = ii()
+for _ in range(t):
+    solve()
+
+
+# 1a  
+# 2b 
+# 3c 
