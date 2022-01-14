82b3dd2d8a76472329fca72b32c71f9caff03a97
Apurv Rathore <thelegend2710@gmail.com>
2022-01-14 22:42:19 +0530

    Add solution for 1353C

---
+++ 1353/C.py
@@ -0,0 +1,9 @@
+t = int(input())
+for _ in range(t):
+    n= int(input())
+    summ=0
+    for i in range(3,n+1):
+        if i%2==1:
+            summ = summ + (i-1)**2
+    summ = summ*2
+    print(summ)
