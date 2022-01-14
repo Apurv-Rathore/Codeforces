cfd611532293f1adf51495ecaedb23e2b8d04532
Apurv Rathore <thelegend2710@gmail.com>
2022-01-14 23:38:50 +0530

    Add solution for 1463A

---
+++ 1463/A.py
@@ -0,0 +1,14 @@
+t = int(input())
+for _ in range (t):
+    l = [int(x) for x in input().split()]
+    l.sort()
+    a,b,c = l
+    n = a+b+c
+    m = min(a,b,c)
+    if (a+b+c)%9==0:
+        if (a+b+c)//9 <= m:
+            print("YES")
+        else:
+                print("NO")
+    else:
+        print("NO")
