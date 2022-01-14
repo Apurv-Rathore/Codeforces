51dff8aba308b5f54989ca6c1a245a97a54fdae2
Apurv Rathore <thelegend2710@gmail.com>
2022-01-14 23:31:35 +0530

    Add solution for 1353E

---
+++ 1353/E.py
@@ -0,0 +1,19 @@
+import sys
+input = lambda: sys.stdin.readline().rstrip("\r\n")
+
+for _ in range(int(input())):
+    n, k = map(int, input().split())
+    a = input()
+    ans, count, total = 0, 0, 0
+    for i in range(k):
+        count = 0
+        for j in range(i, n, k):
+            if a[j] == "1":
+                count += 1
+                total += 1
+            else:
+                count -= 1
+                if count < 0:
+                    count = 0
+            ans = max(ans, count)
+    print(total - ans)
