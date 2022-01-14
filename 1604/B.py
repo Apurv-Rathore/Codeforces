4343441f232c91fd5efaacb2ce8ec6e4444931d4
Apurv Rathore <thelegend2710@gmail.com>
2022-01-14 22:54:59 +0530

    Add solution for 1604B

---
+++ 1604/B.py
@@ -0,0 +1,35 @@
+import sys,os,io
+import math 
+from collections import defaultdict
+def ii():
+    return int(input())
+def li():
+    return list(map(int,input().split()))
+if(os.path.exists('input.txt')):
+    sys.stdin = open("input.txt","r") ; sys.stdout = open("output.txt","w") 
+else:
+    input = io.BytesIO(os.read(0, os.fstat(0).st_size)).readline
+def solve():
+    n = int(input())
+    l = li()
+    x = set(l)
+    x = len(x)
+
+    if len(l)%2==1:
+        if x!=n:
+            print("YES")
+        else:
+            q = l[:]
+            l.sort()
+            if q!=l:
+                print("YES")
+            else:
+                print("NO")
+    else:
+        print("YES")
+
+
+t = 1
+t = ii()
+for _ in range(t):
+    solve()
