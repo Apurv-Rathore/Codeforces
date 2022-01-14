209134ef9d410b8b0958a97ddd52c52e5d7b8d5b
Apurv Rathore <thelegend2710@gmail.com>
2022-01-14 22:54:35 +0530

    Add solution for 1589B

---
+++ 1589/B.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+#define int long long
+using namespace std;
+signed main(){
+    int t;
+    cin>>t;
+    while (t--){
+        int n,m;
+        cin>>n>>m;
+        int res = (n*m)/3;
+        if ((n*m)%3!=0) res++;
+        cout<<res<<endl;
+    }
+    return 0;
+}
