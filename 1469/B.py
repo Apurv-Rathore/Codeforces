091df9a9a77ae2ae6f9d4ae6cc87a4195afa95eb
Apurv Rathore <thelegend2710@gmail.com>
2022-01-14 23:13:46 +0530

    Add solution for 1469B

---
+++ 1469/B.py
@@ -0,0 +1,22 @@
+from sys import stdin
+from math import log,ceil
+t = int(input())
+for _ in range(t):
+    
+    # l = list(map(int,stdin.readline().split()))
+    r1 = int(input())
+    r = list(map(int,input().split()))
+    
+    b1 = int(input())
+    b = list(map(int,input().split()))
+    c = 0
+    m1 = 0
+    for i in range(b1):
+        c = c + b[i]
+        m1 = max(c,m1)
+    c = 0
+    m = 0
+    for i in range(r1):
+        c+=r[i]
+        m = max(m,c)
+    print(m1+m)
