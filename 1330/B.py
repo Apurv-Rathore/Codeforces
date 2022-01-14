0ca8aef94fad5fc8a6d0fa9d68d2b7b2301bc955
Apurv Rathore <thelegend2710@gmail.com>
2022-01-14 23:41:38 +0530

    Add solution for 1330B

---
+++ 1330/B.py
@@ -0,0 +1,22 @@
+def check(a):
+    a.sort()
+    b=[i+1 for i in range(len(a))]
+    if b==a:
+        return 1
+    else:
+        return 0
+ 
+t=int(input())
+for _ in range(t):
+    n=int(input())
+    a=list(map(int,input().split()))
+    b=max(a)
+    c=[]
+    if check(a[:b]) and check(a[b:]):
+        c.append((b,n-b))
+    if check(a[:n-b]) and check(a[n-b:]):
+        c.append((n-b,b))
+    c=list(set(c))
+    print(len(c))
+    for i in range(len(c)):
+        print(*c[i])
