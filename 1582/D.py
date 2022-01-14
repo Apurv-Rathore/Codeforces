afb3ba54c456b2ba64c3d5cab52e43616768d00d
Apurv Rathore <thelegend2710@gmail.com>
2022-01-14 22:31:55 +0530

    Add solution for 1582D

---
+++ 1582/D.py
@@ -0,0 +1,9 @@
+from math import *
+def sign(num):return -1 if num < 0 else 1
+for _ in range(int(input())):
+	n = int(input());l = list(map(int, input().split()))
+	if n % 2:
+		a, b, c, *l = l
+		print(abs(b) * sign(a), (abs(a) + abs(c)) * -sign(b), abs(b) * sign(c), end = ' ')
+	for i in range(0, len(l), 2):print(abs(l[i + 1]) * -sign(l[i]), abs(l[i]) * sign(l[i + 1]), end = ' ')
+	print()
