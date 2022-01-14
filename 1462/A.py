f3f264c0a7d054717251cc77cbbd1d8a0a2d2b6f
Apurv Rathore <thelegend2710@gmail.com>
2022-01-14 23:39:02 +0530

    Add solution for 1462A

---
+++ 1462/A.py
@@ -0,0 +1,20 @@
+from collections import defaultdict
+t = int(input())
+for _ in range(t):
+    n = int(input())
+    a = [int(x) for x in input().split()]
+    l = 0
+    r = n-1
+    ans = [0]*n
+    i = 0
+    while(l<=r and i<n):
+        
+        ans[i]=a[l]
+        if (i==n-1):
+            break
+        ans[i+1]=a[r]
+        l+=1
+        r-=1
+        i+=2
+    
+    print(*ans)
