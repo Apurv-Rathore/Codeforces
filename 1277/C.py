8ad5384e66c0b160b7ea875cb2bd5818f811de71
Apurv Rathore <thelegend2710@gmail.com>
2022-01-14 23:05:16 +0530

    Add solution for 1277C

---
+++ 1277/C.py
@@ -0,0 +1,7 @@
+t = int(input())
+for _ in range(t):
+    s = input()
+    s = 't*o'.join('o*e'.join('tw*ne'.join(s.split('twone')).split('one')).split('two'))
+    l = [str(i+1) for i in range(len(s)) if s[i] == '*']
+    print(len(l))
+    print(' '.join(l))
