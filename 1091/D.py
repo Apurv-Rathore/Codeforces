35377a51d3c9706dcfde3c2a79f6000ceb0b3ab9
Apurv Rathore <thelegend2710@gmail.com>
2022-01-14 23:13:07 +0530

    Add solution for 1091D

---
+++ 1091/D.py
@@ -0,0 +1,11 @@
+fact = [0] * 1000001
+fact[0] = 1
+mod = 998244353
+n = int(input())
+fact[n] = n
+for i in range(n - 1, -1, -1):
+    fact[i] = (fact[i + 1] * i) % mod
+ans = (n * fact[1]) % mod
+for i in range(1, n):
+    ans = (ans + mod - fact[n - (n - i) + 1]) % mod
+print(int(ans))
