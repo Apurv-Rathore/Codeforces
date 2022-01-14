79c0ba54c8444ce9fa54d4e32c2b34c68ae5c64a
Apurv Rathore <thelegend2710@gmail.com>
2022-01-14 22:32:11 +0530

    Add solution for 1579B

---
+++ 1579/B.py
@@ -0,0 +1,14 @@
+# B
+t=int(input())
+for _ in range(t):
+    n=int(input())
+    arr=list(map(int, input().split()))
+    tmp=[]
+    for i in range(n):
+        min_n=min(arr)
+        min_i=arr.index(min_n)
+        if min_i: tmp.append([i+1,i+1+min_i,min_i])
+        del arr[min_i]
+    print(len(tmp))
+    for i in tmp:
+        print(*i)
