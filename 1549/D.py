c37c0ca6f31e0b7c50b53e0a40cd726ed0bb0ad8
Apurv Rathore <thelegend2710@gmail.com>
2022-01-14 22:34:47 +0530

    Add solution for 1549D

---
+++ 1549/D.py
@@ -0,0 +1,223 @@
+# ///////////////////////////////////////////////////////////////////////////
+# //////////////////// PYTHON IS THE BEST ////////////////////////
+# ///////////////////////////////////////////////////////////////////////////
+import sys,os,io
+from sys import stdin
+import math 
+from collections import defaultdict
+from heapq import heappush, heappop, heapify
+from bisect import bisect_left , bisect_right
+from io import BytesIO, IOBase
+BUFSIZE = 8192
+
+class FastIO(IOBase):
+    newlines = 0
+
+    def __init__(self, file):
+        self._fd = file.fileno()
+        self.buffer = BytesIO()
+        self.writable = "x" in file.mode or "r" not in file.mode
+        self.write = self.buffer.write if self.writable else None
+
+    def read(self):
+        while True:
+            b = os.read(self._fd, max(os.fstat(self._fd).st_size, BUFSIZE))
+            if not b:
+                break
+            ptr = self.buffer.tell()
+            self.buffer.seek(0, 2), self.buffer.write(b), self.buffer.seek(ptr)
+        self.newlines = 0
+        return self.buffer.read()
+
+    def readline(self):
+        while self.newlines == 0:
+            b = os.read(self._fd, max(os.fstat(self._fd).st_size, BUFSIZE))
+            self.newlines = b.count(b"\n") + (not b)
+            ptr = self.buffer.tell()
+            self.buffer.seek(0, 2), self.buffer.write(b), self.buffer.seek(ptr)
+        self.newlines -= 1
+        return self.buffer.readline()
+
+    def flush(self):
+        if self.writable:
+            os.write(self._fd, self.buffer.getvalue())
+            self.buffer.truncate(0), self.buffer.seek(0)
+
+
+class IOWrapper(IOBase):
+    def __init__(self, file):
+        self.buffer = FastIO(file)
+        self.flush = self.buffer.flush
+        self.writable = self.buffer.writable
+        self.write = lambda s: self.buffer.write(s.encode("ascii"))
+        self.read = lambda: self.buffer.read().decode("ascii")
+        self.readline = lambda: self.buffer.readline().decode("ascii")
+
+
+
+
+alphabets = list('abcdefghijklmnopqrstuvwxyz')
+
+
+
+#for deep recursion__________________________________________-
+from types import GeneratorType
+def bootstrap(f, stack=[]):
+    def wrappedfunc(*args, **kwargs):
+        if stack:
+            return f(*args, **kwargs)
+        else:
+            to = f(*args, **kwargs)
+            while True:
+                if type(to) is GeneratorType:
+                    stack.append(to)
+                    to = next(to)
+                else:
+                    stack.pop()
+                    if not stack:
+                        break
+                    to = stack[-1].send(to)
+            return to
+
+    return wrappedfunc
+
+def ncr(n, r, p):  
+    num = den = 1
+    for i in range(r):
+        num = (num * (n - i)) % p
+        den = (den * (i + 1)) % p
+    return (num * pow(den,p - 2, p)) % p
+
+def primeFactors(n): 
+    l = []
+    while n % 2 == 0: 
+        l.append(2)
+        n = n / 2
+    for i in range(3,int(math.sqrt(n))+1,2): 
+        while n % i== 0: 
+            l.append(int(i))
+            n = n / i 
+    if n > 2: 
+        l.append(n)
+    # c = dict(Counter(l))
+    return list(set(l))
+    # return c
+
+def power(x, y, p) : 
+	res = 1
+	x = x % p 
+	if (x == 0) : 
+		return 0
+	while (y > 0) : 
+		if ((y & 1) == 1) : 
+			res = (res * x) % p 
+		y = y >> 1	 # y = y/2 
+		x = (x * x) % p 		
+	return res 
+
+#____________________GetPrimeFactors in log(n)________________________________________
+def sieveForSmallestPrimeFactor():
+    MAXN = 100001
+    spf = [0 for i in range(MAXN)]
+    spf[1] = 1
+    for i in range(2, MAXN):
+        spf[i] = i
+    for i in range(4, MAXN, 2):
+        spf[i] = 2
+    for i in range(3, math.ceil(math.sqrt(MAXN))):
+        if (spf[i] == i):
+            for j in range(i * i, MAXN, i): 
+                if (spf[j] == j):
+                    spf[j] = i
+    return spf
+def getPrimeFactorizationLOGN(x):
+    spf = sieveForSmallestPrimeFactor()
+    ret = list()
+    while (x != 1):
+        ret.append(spf[x])
+        x = x // spf[x]  
+    return ret
+#____________________________________________________________
+
+
+
+def SieveOfEratosthenes(n): 
+    #time complexity = nlog(log(n))
+    prime = [True for i in range(n+1)]
+    p = 2
+    while (p * p <= n):
+        if (prime[p] == True):
+            for i in range(p * p, n+1, p):
+                prime[i] = False
+        p += 1
+    return prime
+def si():
+    return input()
+def divideCeil(n,x):
+    if (n%x==0):
+        return n//x
+    return n//x+1
+def ii():
+    return int(input())
+def li():
+    return list(map(int,input().split()))
+
+
+# ///////////////////////////////////////////////////////////////////////////
+# //////////////////// DO NOT TOUCH BEFORE THIS LINE ////////////////////////
+# ///////////////////////////////////////////////////////////////////////////
+if(os.path.exists('input.txt')):
+    sys.stdin = open("input.txt","r") ; sys.stdout = open("output.txt","w") 
+else:
+    sys.stdin, sys.stdout = IOWrapper(sys.stdin), IOWrapper(sys.stdout)
+    input = lambda: sys.stdin.readline().rstrip("\r\n")
+class RangeQuery:
+    def __init__(self, data, func=math.gcd):
+        self.func = func
+        self._data = _data = [list(data)]
+        i, n = 1, len(_data[0])
+        while 2 * i <= n:
+            prev = _data[-1]
+            _data.append([func(prev[j], prev[j + i]) for j in range(n - 2 * i + 1)])
+            i <<= 1
+
+    def query(self, start, stop):
+        """func of data[start, stop)"""
+        depth = (stop - start).bit_length() - 1
+        return self.func(self._data[depth][start], self._data[depth][stop - (1 << depth)])
+
+    def __getitem__(self, idx):
+        return self._data[0][idx]
+
+def fun(mid,s,n):
+    f = 0
+    for i in range(n-mid+1):
+        if s.query(i,i+mid)>1:
+            f = 1
+    return f
+
+
+def solve():
+    n = ii()
+    a = li()
+    d = [abs(a[i+1]-a[i]) for i in range(0,n-1)]
+    s = RangeQuery(d)
+    low = 1
+    high = n-1
+    ans = 0
+    # print(d)
+    while low<=high:
+        mid = (low+high)//2
+        if fun(mid,s,n-1)==1:
+            low = mid+1
+            ans = mid
+        else:
+            high = mid-1
+    
+    print(ans+1)
+
+
+t = 1
+t = ii()
+for _ in range(t):
+    solve()
