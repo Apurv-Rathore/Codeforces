e02f94ad7d2f39f9cb0cd5152d556f0b20c0e6e1
Apurv Rathore <thelegend2710@gmail.com>
2022-01-14 22:32:34 +0530

    Add solution for 1606C

---
+++ 1606/C.py
@@ -0,0 +1,36 @@
+import sys,os,io
+import math 
+from collections import defaultdict
+def ii():
+    return int(input())
+def li():
+    return list(map(int,input().split()))
+if(os.path.exists('input.txt')):
+    sys.stdin = open("input.txt","r") ; sys.stdout = open("output.txt","w") 
+else:
+    input = io.BytesIO(os.read(0, os.fstat(0).st_size)).readline
+
+def solve():
+    n,k = li()
+
+    a = li()
+    x = []
+    k+=1
+    a.sort()
+    for i in range(1,n):
+        x.append(a[i]-a[i-1])
+    ans = 0
+    notes = 0
+    for i in range(len(x)):
+        if k>0:
+            ans += (10**a[i]) * min(10**(x[i])-1,k)
+            k-=min(10**(x[i])-1,k)
+    print(ans+k*(10**a[-1]))
+        
+    
+    
+
+t = 1
+t = ii()
+for _ in range(t):
+    solve()
