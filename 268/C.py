3616d89144554df7ebeb20156019e3eeeebe8529
Apurv Rathore <thelegend2710@gmail.com>
2022-01-14 23:37:33 +0530

    Add solution for 268C

---
+++ 268/C.py
@@ -0,0 +1,11 @@
+n,m = [int(x) for x in input().split()]
+l = []
+a = 0
+b = 1
+k = min(n,m)
+for i in range(min(n,m)+1):
+    l.append([i,k-i])
+    
+print(len(l))
+for i in l:
+    print(*i)
