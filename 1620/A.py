60bfb05c301b7e45444853ed68b05c1a2da0073c
Apurv Rathore <thelegend2710@gmail.com>
2022-01-14 22:53:41 +0530

    Add solution for 1620A

---
+++ 1620/A.py
@@ -0,0 +1,23 @@
+import sys,os,io
+import math 
+from collections import defaultdict
+def ii():
+    return int(input())
+def li():
+    return list(map(int,input().split()))
+if(os.path.exists('input.txt')):
+    sys.stdin = open("input.txt","r") ; sys.stdout = open("output.txt","w") 
+# else:
+#     input = io.BytesIO(os.read(0, os.fstat(0).st_size)).readline
+
+def solve():
+    s = list(input())
+    if s.count('N')==1:
+        print("NO")
+    else:
+        print("YES")
+
+t = 1
+t = ii()
+for _ in range(t):
+    solve()
