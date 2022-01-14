78d45e56b54409d5bfb4a1f7113470322acc9bee
Apurv Rathore <thelegend2710@gmail.com>
2022-01-14 23:38:45 +0530

    Add solution for 1465A

---
+++ 1465/A.py
@@ -0,0 +1,18 @@
+t = int(input())
+for _ in range(t):
+    n = int(input())
+    # l = [int(x) for x in input().split()]
+    l =list(input())
+    l.reverse()
+    c = 0
+    f = 0
+    for i in range(n):
+        if (l[i]==")" and f==0):
+            c+=1
+        else:
+            f = 1
+    if (c>n-c):
+        print("YES")
+    else:
+        print("NO")
+            
