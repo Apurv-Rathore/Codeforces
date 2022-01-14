5cc1368598339ec412039b3be5975f839be930bd
Apurv Rathore <thelegend2710@gmail.com>
2022-01-14 23:12:45 +0530

    Add solution for 479C

---
+++ 479/C.py
@@ -0,0 +1,12 @@
+n = int(input())
+l = []
+for _ in range(n):
+    l.append([int(x) for x in input().split()])
+l.sort()
+c = l[0][1]
+for i in range(n):
+    if (c<=l[i][1]):
+        c =l[i][1]
+    else:
+        c = l[i][0]
+print(c)
