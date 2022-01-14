f3b4e49984076f8c7f0be118741a3076b58c5408
Apurv Rathore <thelegend2710@gmail.com>
2022-01-14 23:13:09 +0530

    Add solution for 1073D

---
+++ 1073/D.py
@@ -0,0 +1,10 @@
+n,m = list(map(int,input().split()))
+l = list(map(int,input().split()))
+minimum = min(l); ans = 0
+while(m>=minimum):
+    c = t = 0
+    for i in l:
+        if (i+t<=m):
+            t+=i;c+=1
+    ans+=(m//t)*c;m%=t
+print(ans)
