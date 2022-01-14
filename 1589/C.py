4f4810e0d3cd832fbb9306002a80a6c05977f111
Apurv Rathore <thelegend2710@gmail.com>
2022-01-14 22:54:38 +0530

    Add solution for 1589C

---
+++ 1589/C.py
@@ -0,0 +1,17 @@
+for _ in range(int(input())):
+    n = int(input())
+    a = [int(x) for x in input().split()]
+    b = [int(x) for x in input().split()]
+    a.sort()
+    b.sort()
+    ok = 1
+    for i in range(n):
+        if a[i]==b[i]:
+            continue
+        if a[i]+1==b[i]:
+            continue
+        ok = 0
+    if ok==0:
+        print("NO")
+    else:
+        print("YES")
