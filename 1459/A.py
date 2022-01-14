64d84a9b0e4fb595369dd0e783060fe836fdbced
Apurv Rathore <thelegend2710@gmail.com>
2022-01-14 22:41:39 +0530

    Add solution for 1459A

---
+++ 1459/A.py
@@ -0,0 +1,17 @@
+from sys import stdin
+for _ in range (int(input())):
+    n = int(input())
+    r = [int(i) for i in list(input())]
+    b = [int(i) for i in list(input())]
+    x=0; y=0
+    for i in range (n):
+        if r[i]>b[i]:
+            x+=1
+        elif r[i]<b[i]:
+            y+=1
+    if x>y:
+        print("RED")
+    elif y>x:
+        print("BLUE")
+    else:
+        print("EQUAL")
