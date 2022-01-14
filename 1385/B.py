aaa8938cd43b5f12543aafd671bbc85c8d4c13b3
Apurv Rathore <thelegend2710@gmail.com>
2022-01-14 23:17:37 +0530

    Add solution for 1385B

---
+++ 1385/B.py
@@ -0,0 +1,11 @@
+t = int(input())
+for i in range(t):
+    n = int(input())
+    l = list(map(int,input().split()))
+    ans = []
+    for e in range(n*2):
+        if l[e] not in ans:
+            ans.append(l[e])
+    ans = list(ans)
+    print(*ans)
+        
