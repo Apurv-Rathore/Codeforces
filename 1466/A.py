f768aa3fc7c588a8e99879dce4ddf040d2be91e8
Apurv Rathore <thelegend2710@gmail.com>
2022-01-14 23:37:49 +0530

    Add solution for 1466A

---
+++ 1466/A.py
@@ -0,0 +1,13 @@
+from math import ceil, log
+from sys import stdin
+t = int(input())
+for _ in range(t):
+    n = int(input())
+    l = [int(x) for x in input().split()]
+    # n,k = list(map(int,stdin.readline().split()))
+    a = []
+    for i in range(n-1):
+        for j in range(i+1,n):
+            a.append(abs(l[j]-l[i]))
+    print(len(set(a)))
+    
