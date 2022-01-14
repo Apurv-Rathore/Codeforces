e14c295d62126804d81241feb53c310a3c148559
Apurv Rathore <thelegend2710@gmail.com>
2022-01-14 22:55:31 +0530

    Add solution for 1536A

---
+++ 1536/A.py
@@ -0,0 +1,17 @@
+for _ in range(int(input())):
+    n = int(input())
+    l = [int(x) for x in input().split()]
+    f = 0
+    for i in range(n):
+        if l[i]<0:
+            print("NO")
+            f = 1
+            break 
+    if f==1:
+        continue
+    print("YES")
+    ans = []
+    for i in range(101):
+        ans.append(i)
+    print(len(ans))
+    print(*ans)
