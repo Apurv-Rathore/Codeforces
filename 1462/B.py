9e827e59321b8d7cfae5756d571fdefffc65b70f
Apurv Rathore <thelegend2710@gmail.com>
2022-01-14 23:38:59 +0530

    Add solution for 1462B

---
+++ 1462/B.py
@@ -0,0 +1,13 @@
+from collections import defaultdict
+t = int(input())
+for _ in range(t):
+    n = int(input())
+    ss = input()
+    if (ss=='2020'):
+        print("YES")
+        continue
+    l = list(ss)
+    if (list("2020")==l[:4] or list("2020")==l[-4:] or (list("2020")==l[:1]+l[-3:]) or (list("2020")==l[:2]+l[-2:]) or (list("2020")==l[:3]+l[-1:])):
+        print("YES")
+    else:
+        print("NO")
