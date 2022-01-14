b0e790ebe3004a2d8dccb182f66ba087a4e5c545
Apurv Rathore <thelegend2710@gmail.com>
2022-01-14 23:12:42 +0530

    Add solution for 441C

---
+++ 441/C.py
@@ -0,0 +1,21 @@
+n,m,k = [int(x) for x in input().split()]
+l = []
+for i in range(n):
+    if (i%2==0):
+        for j in range(m):
+            l.append(i+1)
+            l.append(j+1)
+        
+    else:
+        for j in range(m-1,-1,-1):
+            l.append(i+1)
+            l.append(j+1)
+ans = []
+x = 0
+for i in range(k-1):
+    ans.append(l[x:x+4])
+    x+=4
+ans.append(l[x:])
+for i in ans:
+    print(len(i)//2,end=" ")
+    print(*i)
