df9fde7f88ac1d9f7869c49fa3ab90c552b6eed5
Apurv Rathore <thelegend2710@gmail.com>
2022-01-14 22:39:56 +0530

    Add solution for 607A

---
+++ 607/A.py
@@ -0,0 +1,12 @@
+from bisect import bisect_left as bl
+n = int(input());l = []
+for i in range(n):
+    l.append(tuple(list(map(int,input().split()))))
+l.sort();DP = [0]*(n)
+for i in range(n):
+    x = bl(l,(l[i][0]-l[i][1],0))
+    if x==0:
+        DP[i]=1
+    else:
+        DP[i]=DP[x-1]+1
+print(n-max(DP))
