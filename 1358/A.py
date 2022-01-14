f50d7d4b317dc7e7908bb456ea8e913cbbe4525c
Apurv Rathore <thelegend2710@gmail.com>
2022-01-14 22:42:00 +0530

    Add solution for 1358A

---
+++ 1358/A.py
@@ -0,0 +1,9 @@
+t = int(input())
+for _ in range(t):
+    n , m = [int(x) for x in input().split()]
+    if n%2==0 and m%2==0:
+        print((m*n)//2)
+    elif m%2==1 and n%2==1:
+        print((n*(m-1)//2) + ((n-1)//2)+1)
+    else:
+        print(m*n//2)
