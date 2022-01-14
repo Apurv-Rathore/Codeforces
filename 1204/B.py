1041b001c6d8b64a983cb4e9d2300e71b0917baf
Apurv Rathore <thelegend2710@gmail.com>
2022-01-14 23:03:42 +0530

    Add solution for 1204B

---
+++ 1204/B.py
@@ -0,0 +1,29 @@
+import sys,os,io
+if(os.path.exists('input.txt')):
+    sys.stdin = open("input.txt","r") ; sys.stdout = open("output.txt","w") 
+else:
+    input = io.BytesIO(os.read(0, os.fstat(0).st_size)).readline
+t = 1
+# t = int(input())
+for _ in range(t):
+    n,l,r = [int(x) for x in input().split()]
+    ansMin = 0
+    x = 2
+    for i in range(l-1):
+        ansMin+=x
+        x*=2 
+    ansMin+=1
+    ansMin+=n-l 
+
+
+    ansMax = 0
+    x = 2
+    for i in range(r-1):
+        ansMax+=x
+        x*=2 
+    x//=2
+    ansMax+=1
+    ansMax+=(n-r)*x 
+    print(ansMin,ansMax)
+
+
