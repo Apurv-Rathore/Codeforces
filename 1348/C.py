0e2da90b3d7fa3081ac5d791152e1530b39b8bbe
Apurv Rathore <thelegend2710@gmail.com>
2022-01-14 23:09:56 +0530

    Add solution for 1348C

---
+++ 1348/C.py
@@ -0,0 +1,14 @@
+from sys import stdin
+input=lambda : stdin.readline().strip()
+from math import ceil,sqrt,factorial,gcd
+for _ in range(int(input())):
+	n,k=map(int,input().split())
+	s=sorted(list(input()))
+	if n==k:
+		print(s[-1])
+	elif s[0]!=s[k-1]:
+		print(s[k-1])
+	elif s[-1]==s[k]:
+		print(s[0]+s[k]*((n-1)//k))
+	else:
+		print(*s[k-1:],sep="")
