1f855a3603a457a8b03bb46dbd803c5bdb3054a3
Apurv Rathore <thelegend2710@gmail.com>
2022-01-14 23:37:47 +0530

    Add solution for 1291A

---
+++ 1291/A.py
@@ -0,0 +1,16 @@
+for _ in range(int(input())):
+    n = int(input())
+    # l = list(map(int,input().split()))
+    l = list(input())
+    f = 0
+    for i in range(n-1):
+        if (int(l[i])%2==1):
+            for j in range(i+1,n):
+                if (int(l[j])%2==1):
+                    print(l[i]+l[j])
+                    f = 1
+                    break
+            if (f==1):
+                break
+    if (f==0):
+        print(-1)
