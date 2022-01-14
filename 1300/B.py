58d8f1b518b13083cca1642077a944f5ad2145d6
Apurv Rathore <thelegend2710@gmail.com>
2022-01-14 23:37:06 +0530

    Add solution for 1300B

---
+++ 1300/B.py
@@ -0,0 +1,8 @@
+import sys,os,io
+input = io.BytesIO(os.read(0, os.fstat(0).st_size)).readline
+    
+for _ in range (int(input())):
+    n = int(input())
+    a = [int(i) for i in input().split()]
+    a.sort()
+    print(a[n]-a[n-1])
