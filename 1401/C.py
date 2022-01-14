81e3bec3cfc1323f22ca15673211d87ed2a3a9a5
Apurv Rathore <thelegend2710@gmail.com>
2022-01-14 23:41:43 +0530

    Add solution for 1401C

---
+++ 1401/C.py
@@ -0,0 +1,17 @@
+t = int(input())
+for _ in range(t):
+    n = int(input())
+    l = list(map(int,input().split()))
+    l1  = l[:]
+    l1.sort()
+    ans = "YES"
+    a = min(l)
+    flag = 0
+    for i in range(n):
+        if l1[i]!=l[i]:
+            if l[i]%a!=0:
+                flag = 1
+    if flag==1:
+        print("NO")
+    else:
+        print("YES")
