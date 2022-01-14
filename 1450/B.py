ed3c47f71ce60f678c7d852f120912adeb453009
Apurv Rathore <thelegend2710@gmail.com>
2022-01-14 23:39:33 +0530

    Add solution for 1450B

---
+++ 1450/B.py
@@ -0,0 +1,22 @@
+t = int(input())
+for _ in range(t):
+    n,k = [int(j) for j in input().split()]
+    l = []
+    for i in range(n):
+        x,y = [int(j) for j in input().split()]
+        l.append([x,y])
+    
+
+    
+    f1 = -1
+    for i in range(n):
+        f = 0
+        for j in range(n):
+            if (abs(l[i][0]-l[j][0])+abs(l[i][1]-l[j][1])>k):
+                f = -1
+                break
+        if (f==0):
+            f1 = 1
+            break
+    print(f1)
+                
