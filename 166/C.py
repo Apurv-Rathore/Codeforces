279dc75993004f7745fe0c146d8ae9f1da757a8b
Apurv Rathore <thelegend2710@gmail.com>
2022-01-14 23:38:29 +0530

    Add solution for 166C

---
+++ 166/C.py
@@ -0,0 +1,13 @@
+import bisect
+n,s = [int(x) for x in input().split()]
+l = [int(x) for x in input().split()]
+l.sort()
+x = s
+ans = 0
+while(l[(n+1)//2 -1]!=x):
+    # print(bisect.bisect(l,x))
+    l.insert(bisect.bisect_left(l,x),x)
+    ans+=1
+    n+=1
+    # print(l,n,l[(n+1)//2 -1])
+print(ans)
