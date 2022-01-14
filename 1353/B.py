2e8e2ac8279e7a95d6f1db35287efe3371d08b11
Apurv Rathore <thelegend2710@gmail.com>
2022-01-14 22:42:21 +0530

    Add solution for 1353B

---
+++ 1353/B.py
@@ -0,0 +1,13 @@
+t = int(input())
+for _ in range(t):
+    n,k = [int(x) for x in input().split()]
+    a =  [int(x) for x in input().split()]
+    b = [int(x) for x in input().split()]
+    a.sort()
+    b.sort()
+    b.reverse()
+    for i in range(k):
+        if a[i] < b[i]:
+            a[i]=b[i]
+    print(sum(a))
+    
