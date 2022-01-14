4f24530000a3c7b7c663e01dc016bf16f2144178
Apurv Rathore <thelegend2710@gmail.com>
2022-01-14 23:17:28 +0530

    Add solution for 1382B

---
+++ 1382/B.py
@@ -0,0 +1,19 @@
+t = int(input())
+for _ in range(t):
+    n = int(input())
+    a = [int(x) for x in input().split()]
+    if a.count(1)==n:
+        if n%2==0:
+            print("Second")
+        else:
+            print("First")
+    else:
+        c = 0
+        for i in range(n):
+            if a[i]!=1:
+                c = i
+                break
+        if c%2==1:
+            print("Second")
+        else:
+            print("First")
