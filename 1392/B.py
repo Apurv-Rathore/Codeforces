ffd430c6190e5bcaa76515d6adc25e72c61d112f
Apurv Rathore <thelegend2710@gmail.com>
2022-01-14 23:42:29 +0530

    Add solution for 1392B

---
+++ 1392/B.py
@@ -0,0 +1,16 @@
+t = int(input())
+for _ in range(t):
+    n,k = list(map(int,input().split()))
+    l = list(map(int,input().split()))
+    m = max(l)
+    for i in range(n):
+        l[i] = m - l[i]
+    
+    if k%2==1:
+        print(*l)
+    else:
+        m = max(l)
+        for i in range(n):
+            l[i] = m - l[i]
+        print(*l)
+    
