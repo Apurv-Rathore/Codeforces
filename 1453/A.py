3f47be6b54d6754eb4fa7c372dabf4a87803d039
Apurv Rathore <thelegend2710@gmail.com>
2022-01-14 23:39:40 +0530

    Add solution for 1453A

---
+++ 1453/A.py
@@ -0,0 +1,15 @@
+t = int(input())
+from math import ceil
+from collections import defaultdict
+for _ in range(t):
+    # n = int(input())
+    n,m = [int(x) for x in input().split()]
+    l1 = [int(x) for x in input().split()]
+    l2 = [int(x) for x in input().split()]
+    c = 0
+    for i in l1:
+        if i in l2:
+            c+=1
+    print(c)
+    
+    
