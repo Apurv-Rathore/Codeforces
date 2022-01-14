249798551a206b5eb264cec53aee547ffcabc91d
Apurv Rathore <thelegend2710@gmail.com>
2022-01-14 22:55:03 +0530

    Add solution for 1604A

---
+++ 1604/A.py
@@ -0,0 +1,27 @@
+import sys,os,io
+import math 
+from collections import defaultdict
+def ii():
+    return int(input())
+def li():
+    return list(map(int,input().split()))
+if(os.path.exists('input.txt')):
+    sys.stdin = open("input.txt","r") ; sys.stdout = open("output.txt","w") 
+else:
+    input = io.BytesIO(os.read(0, os.fstat(0).st_size)).readline
+
+def solve():
+    n = ii()
+    a = li()
+    c = 0 
+    for i in range(n):
+        if a[i]>i+1+c:
+            c += (a[i]) - (i+c+1)
+    print(c)
+
+
+t = 1
+t = ii()
+for _ in range(t):
+    solve()
+
