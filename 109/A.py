fa86c4a6a45c280f9bcacc6271269b374646cbc7
Apurv Rathore <thelegend2710@gmail.com>
2022-01-14 23:38:06 +0530

    Add solution for 109A

---
+++ 109/A.py
@@ -0,0 +1,13 @@
+n = int(input())
+f = 0
+for i in range(10000):
+    # x4+y7 = N
+    y7 = n-4*i 
+    if (y7>=0):
+        if (y7%7==0):
+            ans = '4'*i + '7'*(y7//7)
+            f = 1 
+            print(ans)
+            break
+if (f==0):
+    print(-1)
