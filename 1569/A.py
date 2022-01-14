5275cebb0163d4f141fd727cdbf552c7b2f1911d
Apurv Rathore <thelegend2710@gmail.com>
2022-01-14 22:33:51 +0530

    Add solution for 1569A

---
+++ 1569/A.py
@@ -0,0 +1,28 @@
+import sys,os,io
+import math 
+from collections import defaultdict
+def ii():
+    return int(input())
+def li():
+    return list(map(int,input().split()))
+if(os.path.exists('input.txt')):
+    sys.stdin = open("input.txt","r") ; sys.stdout = open("output.txt","w") 
+# else:
+#     input = io.BytesIO(os.read(0, os.fstat(0).st_size)).readline
+
+def solve():
+    n = ii()
+    s = input()
+    for i in range(n-1):
+        if s[i]=='a' and s[i+1]=='b':
+            print(i+1,i+2)
+            return 
+        if s[i]=='b' and s[i+1]=='a':
+            print(i+1,i+2)
+            return 
+    print(-1,-1)
+
+t = 1
+t = ii()
+for _ in range(t):
+    solve()
