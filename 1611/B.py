39702bf24f0b1c4ab9c7b350bbfd71d61846382d
Apurv Rathore <thelegend2710@gmail.com>
2022-01-14 23:28:31 +0530

    Add solution for 1611B

---
+++ 1611/B.py
@@ -0,0 +1,21 @@
+import sys,os,io
+import math 
+from collections import defaultdict
+def ii():
+    return int(input())
+def li():
+    return list(map(int,input().split()))
+if(os.path.exists('input.txt')):
+    sys.stdin = open("input.txt","r") ; sys.stdout = open("output.txt","w") 
+else:
+    input = io.BytesIO(os.read(0, os.fstat(0).st_size)).readline
+
+def solve():
+    a,b = li()
+    ans = min((a+b)//4 , a,b )
+    print(ans)
+
+t = 1
+t = ii()
+for _ in range(t):
+    solve()
