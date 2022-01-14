40322837e6733e668a1def3ba847363861e823fb
Apurv Rathore <thelegend2710@gmail.com>
2022-01-14 23:53:06 +0530

    Add solution for 1132F

---
+++ 1132/F.py
@@ -0,0 +1,91 @@
+# ///////////////////////////////////////////////////////////////////////////
+# //////////////////// PYTHON IS THE BEST ////////////////////////
+# ///////////////////////////////////////////////////////////////////////////
+
+import sys,os,io
+import math 
+from collections import defaultdict
+
+from io import BytesIO, IOBase
+from types import GeneratorType
+BUFSIZE = 8192
+
+class FastIO(IOBase):
+    newlines = 0
+    def __init__(self, file):
+        self._fd = file.fileno()
+        self.buffer = BytesIO()
+        self.writable = "x" in file.mode or "r" not in file.mode
+        self.write = self.buffer.write if self.writable else None
+
+    def read(self):
+        while True:
+            b = os.read(self._fd, max(os.fstat(self._fd).st_size, BUFSIZE))
+            if not b:
+                break
+            ptr = self.buffer.tell()
+            self.buffer.seek(0, 2), self.buffer.write(b), self.buffer.seek(ptr)
+        self.newlines = 0
+        return self.buffer.read()
+
+    def readline(self):
+        while self.newlines == 0:
+            b = os.read(self._fd, max(os.fstat(self._fd).st_size, BUFSIZE))
+            self.newlines = b.count(b"\n") + (not b)
+            ptr = self.buffer.tell()
+            self.buffer.seek(0, 2), self.buffer.write(b), self.buffer.seek(ptr)
+        self.newlines -= 1
+        return self.buffer.readline()
+
+    def flush(self):
+        if self.writable:
+            os.write(self._fd, self.buffer.getvalue())
+            self.buffer.truncate(0), self.buffer.seek(0)
+class IOWrapper(IOBase):
+    def __init__(self, file):
+        self.buffer = FastIO(file)
+        self.flush = self.buffer.flush
+        self.writable = self.buffer.writable
+        self.write = lambda s: self.buffer.write(s.encode("ascii"))
+        self.read = lambda: self.buffer.read().decode("ascii")
+        self.readline = lambda: self.buffer.readline().decode("ascii")
+def ii():
+    return int(input())
+def li():
+    return list(map(int,input().split()))
+
+
+# ///////////////////////////////////////////////////////////////////////////
+# //////////////////// DO NOT TOUCH BEFORE THIS LINE ////////////////////////
+# ///////////////////////////////////////////////////////////////////////////
+if(os.path.exists('input.txt')):
+    sys.stdin = open("input.txt","r") ; sys.stdout = open("output.txt","w") 
+else:
+    sys.stdin, sys.stdout = IOWrapper(sys.stdin), IOWrapper(sys.stdout)
+    input = lambda: sys.stdin.readline().rstrip("\r\n")
+n = ii()
+s = list(input())
+
+dp = [[-1 for i in range(n)] for j in range(n)]
+def rec(i,j):
+    
+    if i>j:
+        return 0
+    if i==j:
+        return 1
+    if i+1==j:
+        if s[i]==s[j]:
+            return 1
+        return 2 
+    if dp[i][j]!=-1:
+        return dp[i][j]
+    ans = rec(i+1,j)+1
+    # ans=min(ans,rec(i,j-1)+1)
+    for k in range(i+1,j+1):
+        if s[k]==s[i]:
+            ans = min(ans, rec(i+1,k-1)+rec(k,j))
+    dp[i][j] = ans
+    return ans
+
+print(rec(0,n-1))
+            
