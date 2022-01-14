4b5d0fc624d7241cfb5b884d8719c21cf0cf84be
Apurv Rathore <thelegend2710@gmail.com>
2022-01-14 22:33:08 +0530

    Add solution for 1602A

---
+++ 1602/A.py
@@ -0,0 +1,25 @@
+import sys,os,io
+import math 
+from collections import defaultdict
+def ii():
+    return int(input())
+def li():
+    return list(map(int,input().split()))
+if(os.path.exists('input.txt')):
+    sys.stdin = open("input.txt","r") ; sys.stdout = open("output.txt","w") 
+# else:
+#     input = io.BytesIO(os.read(0, os.fstat(0).st_size)).readline
+
+def solve():
+    s = input()
+    s = list(s)
+    m = min(s)
+    s.remove(m)
+    print(m,''.join(s))
+
+    
+
+t = 1
+t = ii()
+for _ in range(t):
+    solve()
