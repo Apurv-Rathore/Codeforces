4c4567be767f84c6cca2615bff6818c318a346ca
Apurv Rathore <thelegend2710@gmail.com>
2022-01-14 23:28:35 +0530

    Add solution for 1607B

---
+++ 1607/B.py
@@ -0,0 +1,12 @@
+t = int(input())
+for _ in range(t):
+	x,n=input().split()
+	x=int(x)
+	n=int(n)
+	base=(n//4)*4
+	for n in range(base+1,n+1):
+		if(x%2==0):
+			x=x-n
+		else:
+			x=x+n
+	print(x)
