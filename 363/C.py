86ec1ea6a5c7b6ca757b1675d1a815fadf54b513
Apurv Rathore <thelegend2710@gmail.com>
2022-01-14 23:12:37 +0530

    Add solution for 363C

---
+++ 363/C.py
@@ -0,0 +1,23 @@
+l = list(input())
+c = 1
+res = 0
+n = len(l)
+ans = [l[0]]
+for i in range(1,n):
+    if (l[i]==l[i-1]):
+        if (c==1):
+            if len(ans)>2 and ans[-2]==ans[-3]:
+                continue
+        if (c==2):
+            continue
+        c+=1
+        ans.append(l[i])
+    else:
+        c = 1
+        ans.append(l[i])
+s = ''
+for i in ans:
+    s+=i
+print(s)
+        
+        
