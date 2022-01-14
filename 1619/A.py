216fe6ce61f39953b604ca9eb3ede9354ef604bb
Apurv Rathore <thelegend2710@gmail.com>
2022-01-14 23:28:28 +0530

    Add solution for 1619A

---
+++ 1619/A.py
@@ -0,0 +1,26 @@
+import sys,os,io
+import math 
+from collections import defaultdict
+def ii():
+    return int(input())
+def li():
+    return list(map(int,input().split()))
+if(os.path.exists('input.txt')):
+    sys.stdin = open("input.txt","r") ; sys.stdout = open("output.txt","w") 
+# else:
+#     input = io.BytesIO(os.read(0, os.fstat(0).st_size)).readline
+
+def solve():
+    s = list(input())
+    if len(s)%2==0:
+        x = len(s)//2
+        if s[:x]==s[x:]:
+            print("YES")
+            return 
+    print("NO")
+    return
+
+t = 1
+t = ii()
+for _ in range(t):
+    solve()
