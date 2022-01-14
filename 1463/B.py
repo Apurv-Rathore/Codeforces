d6e478b38532340a019451d31c98cbda1d8088b3
Apurv Rathore <thelegend2710@gmail.com>
2022-01-14 22:41:42 +0530

    Add solution for 1463B

---
+++ 1463/B.py
@@ -0,0 +1,9 @@
+from sys import stdin
+for _ in range (int(input())):
+    n = int(input())
+    a = [int(i) for i in stdin.readline().split()]
+    ans = [a[0]]
+    for i in range (1,n):
+        x = (a[i]//ans[i-1])*ans[i-1]
+        ans.append(max(x, 1))        
+    print(*ans)
