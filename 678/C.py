7274f4a6ccf59271c3d3f8fa9e83409405f38851
Apurv Rathore <thelegend2710@gmail.com>
2022-01-14 23:13:28 +0530

    Add solution for 678C

---
+++ 678/C.py
@@ -0,0 +1,17 @@
+import math
+def lcm(a, b):
+    return abs(a*b) // math.gcd(a, b)
+# t = int(input())
+from sys import stdin
+t = 1
+for _ in range(t):
+    n,a,b,p,q = [int(x) for x in input().split()]
+    # n,k = list(map(int,stdin.readline().split()))
+    x = lcm(a,b)
+    if (p<q):
+        c = ((n//a) - n//x )*p + (n//b)*q
+        print(   c)
+    else:
+        c = ((n//b) - n//x )*q + (n//a)*p
+        print(   c)
+        
