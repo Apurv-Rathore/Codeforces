a8e74b2dfd928bf02c0465d1c512e9485a53004a
Apurv Rathore <thelegend2710@gmail.com>
2022-01-14 23:17:34 +0530

    Add solution for 1385D

---
+++ 1385/D.py
@@ -0,0 +1,28 @@
+import string
+x = 'abcdefghijklmnopqrstuvwxyz'
+
+def func(s,n,i):
+    if len(s)==1:
+        if s==x[i]:
+            return 0
+        else:
+            return 1
+
+    left_count = s[:n//2].count(x[i])
+    right_count = s[n//2:].count(x[i])
+#    print(s[:n//2],s[n//2:])
+#    print(left_count,right_count)
+#    if left_count>right_count:
+        
+#        return (func(s[n//2:],n//2,i+1) + (n//2-(left_count)))
+#    if left_count<right_count:
+#        return (func(s[:n//2],n//2,i+1) + (n//2-(right_count)))
+#    if left_count==right_count:
+    return min((func(s[:n//2],n//2,i+1) + (n//2-(right_count))),(func(s[n//2:],n//2,i+1) + (n//2-(left_count))))
+    
+t = int(input())
+for _ in range(t):
+    n = int(input())
+    s = input()
+    print(func(s,n,0))
+    
