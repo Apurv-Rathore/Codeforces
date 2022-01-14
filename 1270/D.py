8d36bf7a0a376f77548c939b5a6be416fc757181
Apurv Rathore <thelegend2710@gmail.com>
2022-01-14 23:37:53 +0530

    Add solution for 1270D

---
+++ 1270/D.py
@@ -0,0 +1,10 @@
+n,k = [int(j) for j in input().split()]
+from collections import defaultdict
+dic = defaultdict(lambda:0)
+for i in range(k+1):
+    l = [int(j) for j in range(1,k+2)]
+    l.remove(i+1)
+    print('?',*l)
+    x,y = [int(j) for j in input().split()]
+    dic[y]+=1
+print("!",dic[max(dic)])
