8aee86dcf66e96fd1711f95b23dc5834cd7f5f3d
Apurv Rathore <thelegend2710@gmail.com>
2022-01-14 23:30:23 +0530

    Add solution for 1330D

---
+++ 1330/D.py
@@ -0,0 +1,18 @@
+import os,sys
+if(os.path.exists('input.txt')):
+    sys.stdin = open("input.txt","r") ; sys.stdout = open("output.txt","w") 
+from math import log2 as l2
+t = int(input())
+for _ in range(t):
+    d,m = [int(x) for x in input().split()]
+    bit = l2(d)
+    bit = int(bit)+1
+    ans = 1
+    # print("bit",bit)
+    p = 0
+    for i in range(0,bit+1):
+        if (1<<(i))>d:
+            break
+        ans = ans * (min(2**(i+1) -1 , d) - 2**(i) + 2)
+    ans-=1
+    print(ans%m)
