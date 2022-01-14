c4cb838f54437126abfcf09b47da1a66c8447573
Apurv Rathore <thelegend2710@gmail.com>
2022-01-14 23:13:38 +0530

    Add solution for 489C

---
+++ 489/C.py
@@ -0,0 +1,8 @@
+a, b = map(int, input().split())
+if b>9*a or (b == 0 and a != 1):print(-1, -1)
+elif a == 1 and b == 0:print(0, 0)
+else:
+    M, m = 0, 10**(a-1)
+    for i in range(b-1):m += 10 ** (i//9)
+    for i in range(b):M += 10 ** (a-1-i//9)
+    print(m, M)
