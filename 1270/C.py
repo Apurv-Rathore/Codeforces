7696ee4eff8fbf241e3fbb9501be5fb13a506a7f
Apurv Rathore <thelegend2710@gmail.com>
2022-01-14 23:13:33 +0530

    Add solution for 1270C

---
+++ 1270/C.py
@@ -0,0 +1,10 @@
+t = int(input())
+for _ in range(t):
+    n = int(input())
+    l = [int(j) for j in input().split()]
+    x = 0
+    for i in l:
+        x^=i
+    print(2)
+    print(x+sum(l))
+    print(x)
