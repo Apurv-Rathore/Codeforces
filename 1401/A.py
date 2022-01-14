771fe46662eb9bb06d6d98616f4875ddb4bc506b
Apurv Rathore <thelegend2710@gmail.com>
2022-01-14 23:42:20 +0530

    Add solution for 1401A

---
+++ 1401/A.py
@@ -0,0 +1,12 @@
+t = int(input())
+for _ in range(t):
+#    n = int(input())
+    n,k = list(map(int,input().split()))
+    if n>=k:
+        if (n+k)%2==0:
+            print(0)
+        else:
+            print(1)
+    else:
+        print(abs(k-n))
+    
