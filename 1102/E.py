21773b3b975419a50f26ff8a1b46db8bb1f35d21
Apurv Rathore <thelegend2710@gmail.com>
2022-01-14 23:13:03 +0530

    Add solution for 1102E

---
+++ 1102/E.py
@@ -0,0 +1,33 @@
+from collections import defaultdict
+mod = 998244353 
+n = int(input())
+l = list(map(int,input().split()))
+
+dicmax = defaultdict(lambda:-1)
+dicmin = defaultdict(lambda:-1)
+
+for i in range(n):
+    if (dicmin[l[i]]==-1):
+        dicmin[l[i]]=i
+for i in range(n-1,-1,-1):
+    if (dicmax[l[i]]==-1):
+        dicmax[l[i]]=i        
+arr = []
+for i in set(l):
+    arr.append([dicmin[i],dicmax[i]])
+arr.sort()
+c = 0
+length = 0
+cur = 0
+res = 1
+m = -1
+for i in range(0,len(arr)):
+    if (arr[i][0]>m):
+        c+=1
+    m = max(m,arr[i][1])
+
+for i in range(c-1):
+    res = (res*2)%mod
+print(res%mod)
+    
+
