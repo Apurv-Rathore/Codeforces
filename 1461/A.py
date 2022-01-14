0d732aaa9b5906b5a4abbc3229a68f730ecf6480
Apurv Rathore <thelegend2710@gmail.com>
2022-01-14 23:39:15 +0530

    Add solution for 1461A

---
+++ 1461/A.py
@@ -0,0 +1,13 @@
+t = int(input())
+for _ in range(t):
+    n,k = [int(x) for x in input().split()]
+    ans = ''
+    x = ['a','b','c']
+    j = 0
+    for i in range(n):
+        
+        ans+=x[j]
+        j+=1
+        j=j%3
+    print(ans)
+    
