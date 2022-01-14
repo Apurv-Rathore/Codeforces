ec1206061032a01b91af18d4a14f939249a922e4
Apurv Rathore <thelegend2710@gmail.com>
2022-01-14 23:38:41 +0530

    Add solution for 1228A

---
+++ 1228/A.py
@@ -0,0 +1,20 @@
+from sys import stdin
+# t = int(input())
+def check (n):
+    n = list(str(n))
+    if (len(set(n))==len(n)):
+        return True 
+    return False
+t = 1
+for _ in range(t):
+    # n = int(input())
+    l,r = [int(x) for x in stdin.readline().split()]
+    f = 0
+    for i in range(l,r+1):
+        if (check(i)):
+            print(i)
+            f = 1
+            break
+    if (f==0):
+        print(-1)
+    
