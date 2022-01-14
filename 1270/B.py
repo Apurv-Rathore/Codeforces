ee5522e9420718bbe26ac4c44a50c2fbd17a5430
Apurv Rathore <thelegend2710@gmail.com>
2022-01-14 23:37:55 +0530

    Add solution for 1270B

---
+++ 1270/B.py
@@ -0,0 +1,12 @@
+for i in range(int(input())):
+	n = int(input())
+	a = [int(j) for j in input().split()]
+	flag = True
+	for i in range(len(a)-1):
+		if abs(a[i+1]-a[i]) > 1 :
+			print('YES')
+			print(i+1,i+2)
+			flag = False
+			break
+	if flag == True:
+		print('NO')
