189c6cdeb3cefeed53b949fd241f1025d13fb2f9
Apurv Rathore <thelegend2710@gmail.com>
2022-01-14 23:14:09 +0530

    Add solution for 9C

---
+++ 9/C.py
@@ -0,0 +1,20 @@
+n = int(input())
+ans = 0
+l = set([])
+for i in range(2**10+1):
+    t = '0'*10
+    t = list(t)
+    for j in range(0,10):
+        if ((i&(2**j))>0):
+            t[j]=1 
+    t1 = t[:]
+    t = ''
+    for k in t1:
+        t+=str(k)
+    l.add(int(t))
+
+for i in l:
+    if (i<=n and i>0):
+        
+        ans+=1 
+print(ans)
