63162ed40a0b086d43b113d039648bb83ea0203b
Apurv Rathore <thelegend2710@gmail.com>
2022-01-14 23:38:38 +0530

    Add solution for 1228B

---
+++ 1228/B.cpp
@@ -0,0 +1,23 @@
+
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+ll mod=1e9+7,ans=1;
+ll c[1009],r[1009];
+int main()
+{
+ 
+ll w,h;
+cin>>h>>w;
+for(int i=1;i<=h;i++)cin>>r[i];
+for(ll i=1;i<=w;i++)cin>>c[i];
+for(int i=1;i<=h;i++)
+    for(ll j=1;j<=w;j++)
+{
+    if((i-1)==c[j]&&(j-1)<r[i]){cout<<0;return 0;}
+    if((j-1)==r[i]&&(i-1)<c[j]){cout<<0;return 0;}
+     if(((i-1)>c[j])&&(j-1)>r[i])ans=(ans*2)%mod;
+}
+cout<<ans;
+    return 0;
+}
