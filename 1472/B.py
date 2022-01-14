760cd72f93a859b542c900dcb6ef5082f578d37c
Apurv Rathore <thelegend2710@gmail.com>
2022-01-14 23:37:39 +0530

    Add solution for 1472B

---
+++ 1472/B.py
@@ -0,0 +1,20 @@
+from math import ceil,log
+from collections import defaultdict
+from sys import stdin
+# l = list(map(int,stdin.readline().split()))
+t = int(input())
+for _ in range(t):
+    n = int(input())
+    l = list(map(int,input().split()))
+    if (sum(l)%2==0):
+        if (l.count(2)%2==0):
+            print("YES")
+        else:
+            if (l.count(1)!=0):
+                print("YES")
+            else:
+                print("NO")
+    else:
+        print("NO")
+    
+    
