760cd0c19720b716f40cdeed7024b09b536929ec
Apurv Rathore <thelegend2710@gmail.com>
2022-01-14 23:29:40 +0530

    Add solution for 1209D

---
+++ 1209/D.py
@@ -0,0 +1,14 @@
+N, K = [int(x) for x in input().split(' ')]
+f = list(range(N))
+def find(x):
+    if x != f[x]:
+        f[x] = find(f[x])
+    return f[x]
+res = 0
+for _ in range(K):
+    a, b = [int(x)-1 for x in input().split(' ')]
+    fa, fb = find(a), find(b)
+    if fa != fb:
+        res += 1
+        f[fa] = fb
+print(K-res)
