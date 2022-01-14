4883976058aded1351dbf0a4c9c73d406ed80463
Apurv Rathore <thelegend2710@gmail.com>
2022-01-14 22:42:26 +0530

    Add solution for 1353A

---
+++ 1353/A.py
@@ -0,0 +1,16 @@
+t = int(input())
+for _ in range(t):
+    length , summ = [int(x) for x in input().split()]
+    
+    if length==1:
+        print(0)
+        continue
+    if length%2==1:
+        print(2*summ)
+    else:
+        if length==2:
+            print(summ)
+        else:
+            print((2*summ))
+        
+    
