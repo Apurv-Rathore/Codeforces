7b5b3a31080f7e7fc53c15bacbd658470b7b6b32
Apurv Rathore <thelegend2710@gmail.com>
2022-01-14 22:42:10 +0530

    Add solution for 1360B

---
+++ 1360/B.py
@@ -0,0 +1,17 @@
+t = int(input())
+for _ in range(t):
+    n = int(input())
+    l = [int(x) for x in input().split()]
+    l.sort()
+    minimum=100000000
+    x=[]
+    for i in range(n-1):
+        diff = l[i+1]-l[i]
+
+        x.append(diff)
+    print(min(x))
+#        if l[i+1]-l[i]<minimum:
+#            print(l[i+1]-l[i])
+#            minumim = l[i+1]-l[i]
+#    print(minimum)
+#        
