294913a5d21b586bde8211ec275f6ea05c76fe3a
Apurv Rathore <thelegend2710@gmail.com>
2022-01-14 22:41:19 +0530

    Add solution for 1473B

---
+++ 1473/B.py
@@ -0,0 +1,93 @@
+#########################################################################################################\
+#########################################################################################################
+###################################The_Apurv_Rathore#####################################################
+#########################################################################################################
+#########################################################################################################
+
+import sys,os,io
+from math import log, gcd, ceil
+from collections import defaultdict, deque, Counter
+from heapq import heappush, heappop
+
+
+import math 
+
+
+def lcm(x, y):
+    return x * y // gcd(x, y)
+
+def ncr(n, r, p):  #using fermat's little theorem
+    num = den = 1
+    for i in range(r):
+        num = (num * (n - i)) % p
+        den = (den * (i + 1)) % p
+    return (num * pow(den,
+                      p - 2, p)) % p
+
+  
+def primeFactors(n): 
+    l = []
+    while n % 2 == 0: 
+        l.append(2)
+        n = n / 2
+          
+    for i in range(3,int(math.sqrt(n))+1,2): 
+          
+
+        while n % i== 0: 
+            l.append(int(i))
+            n = n / i 
+    if n > 2: 
+        l.append(n)
+    return list(set(l))
+    
+def power(x, y, p) : 
+	res = 1
+	x = x % p 
+	if (x == 0) : 
+		return 0
+	while (y > 0) : 
+		if ((y & 1) == 1) : 
+			res = (res * x) % p 
+		y = y >> 1	 # y = y/2 
+		x = (x * x) % p 		
+	return res 
+
+
+def si():
+    return input()
+
+
+# input = io.BytesIO(os.read(0, os.fstat(0).st_size)).readline
+
+def prefix_sum(arr):
+    r = [0] * (len(arr)+1)
+    for i, el in enumerate(arr):
+        r[i+1] = r[i] + el
+    return r
+
+def divideCeil(n,x):
+    if (n%x==0):
+        return n//x
+    return n//x+1
+
+def ii():
+    return int(input())
+
+def li():
+    return list(map(int,input().split()))
+
+
+t = int(input())
+for _ in range(t):
+    s = input()
+    t = input()
+    if (len(s)>len(t)):
+        s,t = t,s
+    length = lcm(len(list(s)),len(list(t)))
+    a = s*(length//len(s))
+    b = t*(length//len(t))
+    if (a==b):
+        print(a)
+    else:
+        print(-1)
