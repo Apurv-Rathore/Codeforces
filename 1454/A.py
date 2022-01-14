2ba9cad1e095d1a430aa9a8392dc4ef0d5a0a271
Apurv Rathore <thelegend2710@gmail.com>
2022-01-14 23:39:48 +0530

    Add solution for 1454A

---
+++ 1454/A.py
@@ -0,0 +1,11 @@
+import math
+import sys
+from sys import stdin
+t=int(input())
+for _ in range(t):
+    n = int(input())
+    l = [int(x) for x in range(1,n+1)]
+    if (n%2==1):
+        l[n//2],l[0] = l[0],l[n//2]
+    l.reverse()
+    print(*l)
