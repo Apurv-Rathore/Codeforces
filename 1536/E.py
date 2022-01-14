2518d0f03cca1f530146409348110304a002aa89
Apurv Rathore <thelegend2710@gmail.com>
2022-01-14 22:55:21 +0530

    Add solution for 1536E

---
+++ 1536/E.py
@@ -0,0 +1,5 @@
+for _ in range(int(input())):
+    n,m  = [int(X) for X in input().split()];aaa = 0
+    for i in range(n):
+        x = input();aaa+=x.count('#')
+    print(pow(2,aaa,int(1e9+7)) - (1 if aaa==n*m else 0))
