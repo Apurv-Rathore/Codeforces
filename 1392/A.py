442ef6ee869d7a46d49d697cd1268fddc2995759
Apurv Rathore <thelegend2710@gmail.com>
2022-01-14 23:42:31 +0530

    Add solution for 1392A

---
+++ 1392/A.py
@@ -0,0 +1,11 @@
+t = int(input())
+for _ in range(t):
+    n = int(input())
+    l = list(map(int,input().split()))
+    if len(set(l))==1:
+        print(n)
+        continue
+    if n==2 and l[0]==l[1]:
+        print(2)
+    else:
+        print(1)
