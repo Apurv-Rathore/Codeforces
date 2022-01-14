e66568825dd1ae424c0c7c3d3f504df84cdbb313
Apurv Rathore <thelegend2710@gmail.com>
2022-01-14 22:34:05 +0530

    Add solution for 1562A

---
+++ 1562/A.py
@@ -0,0 +1,27 @@
+import sys,os,io
+import math 
+from collections import defaultdict
+def ii():
+    return int(input())
+def li():
+    return list(map(int,input().split()))
+if(os.path.exists('input.txt')):
+    sys.stdin = open("input.txt","r") ; sys.stdout = open("output.txt","w") 
+else:
+    input = io.BytesIO(os.read(0, os.fstat(0).st_size)).readline
+
+def solve():
+    l,r = li()
+    x = r//2 +1
+    if l<=x:
+        print(r%x)
+    else:
+        print(r%l)
+
+
+t = 1
+t = ii()
+for _ in range(t):
+    solve()
+
+
