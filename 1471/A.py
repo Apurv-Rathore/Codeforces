16db93fd5c97bf68205b37049b5cca5294a7cedf
Apurv Rathore <thelegend2710@gmail.com>
2022-01-14 23:37:29 +0530

    Add solution for 1471A

---
+++ 1471/A.py
@@ -0,0 +1,13 @@
+import sys,os,io
+from math import ceil,log
+# input = io.BytesIO(os.read(0, os.fstat(0).st_size)).readline
+t = int(input())
+for _ in range(t):
+    # n = int(input())
+    n,x = list(map(int,input().split()))
+    l = list(map(int,input().split()))
+    m = 0
+    for i in l:
+        m+=ceil(i/x)
+    s = ceil(sum(l)/x)
+    print(s,m)
