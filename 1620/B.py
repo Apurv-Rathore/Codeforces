b54abea58b546028c6b28d48ab2f73e87c76a631
Apurv Rathore <thelegend2710@gmail.com>
2022-01-14 22:38:27 +0530

    Add solution for 1620B

---
+++ 1620/B.py
@@ -0,0 +1,41 @@
+import sys,os,io
+import math 
+from collections import defaultdict
+def ii():
+    return int(input())
+def li():
+    return list(map(int,input().split()))
+if(os.path.exists('input.txt')):
+    sys.stdin = open("input.txt","r") ; sys.stdout = open("output.txt","w") 
+else:
+    input = io.BytesIO(os.read(0, os.fstat(0).st_size)).readline
+
+def solve():
+    w,h = li()
+    x0 = li()[1:]
+    x1 = li()[1:]
+    y0 = li()[1:]
+    y1 = li()[1:]
+    ans = 0
+    x = max(x0)-min(x0)
+    x = x * h 
+    ans = max(ans ,x )
+
+    x = max(x1)-min(x1)
+    x = x * h 
+    ans = max(ans ,x )
+
+    x = max(y0)-min(y0)
+    x = x * w
+    ans = max(ans ,x )
+
+    x = max(y1)-min(y1)
+    x = x * w
+    ans = max(ans ,x )
+
+    print(ans)
+
+t = 1
+t = ii()
+for _ in range(t):
+    solve()
