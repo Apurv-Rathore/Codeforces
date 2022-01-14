146478f951ab298e8389da280fded44de70c685b
Apurv Rathore <thelegend2710@gmail.com>
2022-01-14 23:38:22 +0530

    Add solution for 1140C

---
+++ 1140/C.py
@@ -0,0 +1,15 @@
+import heapq
+n,k=map(int,input().split())
+a=sorted(tuple( list(map(int,input().split())) for  i in range(n)),key= lambda x : x[1],reverse=True)
+time=[]
+m=0
+s=0
+
+for t,b in a:
+    s+=t
+    heapq.heappush(time,t)
+    if len(time)>k:
+        s-=heapq.heappop(time)
+    m=max(m,s*b)
+print(m)    
+    
