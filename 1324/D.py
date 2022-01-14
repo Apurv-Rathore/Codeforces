a797dd4080cc807cdfd08afc32988ac832c602a1
Apurv Rathore <thelegend2710@gmail.com>
2022-01-14 23:10:40 +0530

    Add solution for 1324D

---
+++ 1324/D.py
@@ -0,0 +1,24 @@
+import bisect
+n=int(input())
+a=[int(x) for x in input().split()]
+b=[int(x) for x in input().split()]
+x=[0]*n
+for i in range(len(a)):
+	x[i]=a[i]-b[i]
+x.sort()
+ans=0
+for i in range(n):
+	if x[i]<=0:
+		continue
+	else:
+		ans+=i-bisect.bisect_left(x,-x[i]+1)
+print(ans)
+
+
+
+
+
+
+
+
+
