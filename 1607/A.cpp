98c31fa46fab1a483535ef882107a4028272d2b7
Apurv Rathore <thelegend2710@gmail.com>
2022-01-14 23:28:38 +0530

    Add solution for 1607A

---
+++ 1607/A.cpp
@@ -0,0 +1,43 @@
+#include <bits/stdc++.h>
+#define fastio ios_base::sync_with_stdio(false), cin.tie(NULL);
+#define int long long
+#define rep(i,n) for (int i=0;i<n;i++)
+#define repx(i,x,n) for (int i=x;i<n;i++)
+#define br cout<<endl
+#define vi vector<int>
+using namespace std;
+
+void solve(){
+    string s;
+    cin>>s;
+    int n = s.size();
+    map<char,int> m;
+    rep(i,n){
+        m[s[i]] = i;
+    }
+    string t;
+    cin>>t;
+    int ans = 0;
+    int prev = m[t[0]];
+    repx(i,1,t.size()){
+        ans += abs(prev - m[t[i]]);
+        prev = m[t[i]];
+    }
+    cout<<ans<<endl;
+}   
+
+
+
+signed main(){
+    #ifndef ONLINE_JUDGE
+        freopen("input.txt", "r", stdin);
+        freopen("output.txt", "w", stdout);
+    #endif
+    fastio
+    int t;
+    t = 1;
+    cin>>t;
+    while(t--){
+		solve();
+	}
+}
