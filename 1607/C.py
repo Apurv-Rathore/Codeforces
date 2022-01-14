6c3588d140af6d367e5ddc2b217efaf18070fa9b
Apurv Rathore <thelegend2710@gmail.com>
2022-01-14 23:28:33 +0530

    Add solution for 1607C

---
+++ 1607/C.py
@@ -0,0 +1,35 @@
+import sys,os,io
+import math 
+from collections import defaultdict
+from heapq import heappush, heappop, heapify
+def ii():
+    return int(input())
+def li():
+    return list(map(int,input().split()))
+if(os.path.exists('input.txt')):
+    sys.stdin = open("input.txt","r") ; sys.stdout = open("output.txt","w") 
+else:
+    input = io.BytesIO(os.read(0, os.fstat(0).st_size)).readline
+
+def solve():
+    n = ii()
+    l = li()
+    l.sort()
+    ans = -100000000000000000000000000
+    m = 0
+    for i in range(n):
+        x = l[i]
+        y = x
+        # print(l[i],m,x,x-m)
+        x-=m 
+        ans = max(ans , x)
+        m += x 
+    print(ans)
+
+
+
+
+t = 1
+t = ii()
+for _ in range(t):
+    solve()
