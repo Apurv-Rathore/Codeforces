2e389be573afcf77dac0e81eb7b30cb7b5423fc1
Apurv Rathore <thelegend2710@gmail.com>
2022-01-14 23:39:18 +0530

    Add solution for 1358C

---
+++ 1358/C.py
@@ -0,0 +1,10 @@
+import math
+from math import factorial
+t = int(input())
+for _ in range(t):
+    x1,y1,x2,y2 = [int(x) for x in input().split()]
+    s = abs(x1-x2)+abs(y1-y2)
+    c = abs(x1-x2)
+    cc = s-c
+    ans = cc*c+1
+    print(int(ans))
