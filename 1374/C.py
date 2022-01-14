558db12f8565ff275dea073d2ef98cd920c9aceb
Apurv Rathore <thelegend2710@gmail.com>
2022-01-14 23:17:56 +0530

    Add solution for 1374C

---
+++ 1374/C.py
@@ -0,0 +1,22 @@
+import string
+t = int(input())
+for _ in range(t):
+    n = int(input())
+    s = input()
+    openn=0
+    close=0
+    wrong=0
+    for i in s:
+        if i=="(":
+            openn+=1
+        elif i==")":
+            close+=1
+        if close>openn:
+            wrong+=close-openn
+            openn=0
+            close=0
+        if close==openn:
+            openn=0
+            close=0
+    print(wrong)
+            
