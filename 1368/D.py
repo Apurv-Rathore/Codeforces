372596ea6ea32dd0db0446ae9fa488f68170d2e4
Apurv Rathore <thelegend2710@gmail.com>
2022-01-14 23:36:57 +0530

    Add solution for 1368D

---
+++ 1368/D.py
@@ -0,0 +1,18 @@
+n = int(input())
+l = list(map(int,input().split()))
+b  = [0]*21; ans = 0
+for i in l:
+    x = bin(i)[2:][::-1]
+    for j in range(len(x)):
+        b[j]+=int(x[j])
+while(1):
+    f = 0; s = ''
+    for i in range(20,-1,-1):
+        if (b[i]>0):
+            s+='1';b[i]-=1
+        else:
+            s+='0'
+    ans+=int(s,2)*int(s,2)
+    if (int(s,2)==0):
+        break
+print(ans)
