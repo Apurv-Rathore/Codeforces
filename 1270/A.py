cda34f9a2e66dda8f7029b5c4a41463838f330c0
Apurv Rathore <thelegend2710@gmail.com>
2022-01-14 23:13:36 +0530

    Add solution for 1270A

---
+++ 1270/A.py
@@ -0,0 +1,11 @@
+t = int(input())
+for _ in range(t):
+    
+    a,b,c = [int(x) for x in input().split()]
+    a = [int(x) for x in input().split()]
+    b = [int(x) for x in input().split()]
+    if (max(a)>max(b)):
+        print("YES")
+    else:
+        print("NO")
+    
