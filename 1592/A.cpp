34ad7651d63cddc2e7e0bde395108868e002bd12
Apurv Rathore <thelegend2710@gmail.com>
2022-01-14 22:33:46 +0530

    Add solution for 1592A

---
+++ 1592/A.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+#define fastio ios_base::sync_with_stdio(false), cin.tie(NULL);
+#define int long long
+using namespace std;
+void solve(){
+	int n,H;
+    cin>>n>>H;
+    vector<int> a(n,0);
+    for (int i=0;i<n;i++) cin>>a[i];
+    sort(a.begin(),a.end());
+    int aa = a[n-1] + a[n-2];
+    int cnt = (H/aa)*2;
+    // cout<<H<<" "<<cnt<<endl;
+    if (H%aa>0){
+        if (H%aa>a[n-1])cnt+=2;
+        else cnt++;
+    }
+    cout<<cnt<<endl;
+}
+
+signed main(){
+    #ifndef ONLINE_JUDGE
+        freopen("input.txt", "r", stdin);
+        freopen("output.txt", "w", stdout);
+    #endif
+    fastio
+    int t;
+    t = 1;
+    cin>>t;
+    while(t--){
+		solve();
+	}
+}
