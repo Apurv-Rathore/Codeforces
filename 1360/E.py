91b8c9c02bd1c4176b8b1455abef82888088bed6
Apurv Rathore <thelegend2710@gmail.com>
2022-01-14 22:42:03 +0530

    Add solution for 1360E

---
+++ 1360/E.py
@@ -0,0 +1,29 @@
+t = int(input())
+for _ in range(t):
+    n = int(input())
+    l=[]
+    flag=1
+    for i in range(n):
+        x = [int(x) for x in input()]
+        l.append(x)
+    for i in range(n):
+        if flag==0:
+            break
+        for j in range(n):
+            if flag==0:
+                break
+            if l[i][j]==1:
+                if j==n-1 or i==n-1:
+                    continue
+                elif l[i+1][j]==1 or l[i][j+1]==1:
+                    continue
+                else:
+                    flag=0
+                    print("NO")
+                    break
+    if flag==1:
+        print("YES")
+                
+                
+                
+            
