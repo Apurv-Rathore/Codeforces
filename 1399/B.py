fcb1bcf8e3767bf8ad75337f1cd916e28cdebca7
Apurv Rathore <thelegend2710@gmail.com>
2022-01-14 23:17:06 +0530

    Add solution for 1399B

---
+++ 1399/B.py
@@ -0,0 +1,17 @@
+t = int(input())
+for _ in range(t):
+    n = int(input())
+    a = list(map(int,input().split()))
+    b = list(map(int,input().split()))
+    diffa = []
+    diffb = []
+    ma = min(a)
+    mb = min(b)
+    for i in range(n):
+        diffa.append(a[i]-ma)
+        diffb.append(b[i]-mb)
+    
+    ans = 0
+    for i in range(n):
+        ans+=max(diffa[i],diffb[i])
+    print(ans)
