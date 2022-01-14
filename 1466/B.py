42abf1c86857b7ff904c9e47fb5e0825d2194419
Apurv Rathore <thelegend2710@gmail.com>
2022-01-14 23:13:25 +0530

    Add solution for 1466B

---
+++ 1466/B.py
@@ -0,0 +1,12 @@
+from math import ceil, log
+from sys import stdin
+t = int(input())
+for _ in range(t):
+    n = int(input())
+    l = [int(x) for x in input().split()]
+    # n,k = list(map(int,stdin.readline().split()))
+    l.sort()
+    for i in range(1,n):
+        if (l[i]<=l[i-1]):
+            l[i]+=1
+    print(len(set(l)))
