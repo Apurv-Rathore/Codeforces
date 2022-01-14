e3646e159116644635d348fdb73b50f0a72eb626
Apurv Rathore <thelegend2710@gmail.com>
2022-01-14 23:39:21 +0530

    Add solution for 1363C

---
+++ 1363/C.py
@@ -0,0 +1,15 @@
+t = int(input())
+for _ in range(t):
+    n,x1 = [int(x) for x in input().split()]
+    cnt = 0
+    for i  in range(n-1):
+        u,v = [int(x) for x in input().split()]
+        if (u==x1 or v==x1):
+            cnt+=1
+    if (cnt==1 or n==1):
+        print("Ayush")
+    else:
+        if ((n-1)%2==0):
+            print("Ashish")
+        else:
+            print("Ayush")
