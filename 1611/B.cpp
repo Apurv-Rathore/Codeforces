a6784cc086f9b104c92acf1431a6ba926a8d5684
Apurv Rathore <thelegend2710@gmail.com>
2022-01-14 22:54:26 +0530

    Add solution for 1611B

---
+++ 1611/B.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+#define fastio ios_base::sync_with_stdio(false), cin.tie(NULL);
+#define int long long
+#define rep(i,n) for (int i=0;i<n;i++)
+#define repi(i,x,n) for (int i=x;i<n;i++)
+#define br cout<<endl
+#define vi vector<int>
+using namespace std;
+void solve(){
+    int a,b;
+    cin>>a>>b;
+    int ans = min( (a+b)/4,min( a,b));
+    cout<<ans<<endl;
+
+}
+
+signed main(){
+    #ifndef ONLINE_JUDGE
+        freopen("input.txt", "r", stdin);
+        freopen("output.txt", "w", stdout);
+    #endif
+    fastio
+    int t;
+    t = 1;
+    cin>>t;
+    while(t--){
+		solve();
+        
+	}
+}
