d8922fdecccca0b82ddf95acb8a9f95c305afba4
Apurv Rathore <thelegend2710@gmail.com>
2022-01-14 23:03:26 +0530

    Add solution for 1457B

---
+++ 1457/B.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#define int long long
+using namespace std;
+
+
+void solve()
+{
+    int n,k;
+    cin>>n>>k;
+    int l[n];
+    for(int i=0;i<n;i++) cin>>l[i];
+    int a = 10000000000000;
+    for (int i=1;i<=100;i++)
+    {
+        int an = 0;
+        for (int j=0;j<n;j++)
+        {
+            if(l[j]!=i)
+            {
+                an++;
+                j+=k-1;
+            }
+        }
+        a = min(a,an);
+    }
+    cout<<a<<endl;
+    
+}
+
+signed main()
+{
+   int t;
+   cin>>t;
+   while(t--)
+   {
+   solve();    
+   }
+   
+   
+   return 0;
+}
