e7ee8a0a2f9b516de51d559a4ae6d784335c471c
Apurv Rathore <thelegend2710@gmail.com>
2022-01-14 22:57:05 +0530

    Add solution for 1509C

---
+++ 1509/C.py
@@ -0,0 +1,4 @@
+n = int(input());dp = [[0]*n for j in range(n)];s = [int(x) for x in input().split()];s.sort()
+for i in range(n-1,-1,-1):
+    for j in range(i+1,n):dp[i][j] = min(dp[i+1][j],dp[i][j-1])+s[j]-s[i]
+print(dp[0][n-1])
