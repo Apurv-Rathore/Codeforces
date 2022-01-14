5f7b4fd9941a8685205b7bec3e932439986f3e9e
Apurv Rathore <thelegend2710@gmail.com>
2022-01-14 23:13:00 +0530

    Add solution for 1111B

---
+++ 1111/B.py
@@ -0,0 +1,13 @@
+n,k,m = list(map(int,input().split()))
+l = list(map(int,input().split()))
+res = 0;avg = sum(l)/n;s = sum(l)
+l.sort()
+for i in range(min(n,m+1)):
+    if (i>0):
+        s-=l[i-1]
+    left = m - i
+    temp = min(left,k*(n-i))
+    
+    res = max (res , (s+temp)/(n-i) )
+    
+print(res)
