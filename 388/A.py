ab1227ecbc985b33ae5beb2897ca0ee48205a558
Apurv Rathore <thelegend2710@gmail.com>
2022-01-14 23:14:11 +0530

    Add solution for 388A

---
+++ 388/A.py
@@ -0,0 +1,15 @@
+n = int(input())
+l = [int(x) for x in input().split()]
+ans = 0
+l.sort()
+while(l):
+    l1 = []
+    c = 0
+    for i in range(len(l)):
+        if (l[i]>=c):
+            c+=1
+        else:
+            l1.append(l[i])
+    l = l1[:]
+    ans+=1 
+print(ans)
