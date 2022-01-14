68d96dbe72d340a42ed98b2f263209c9abba110d
Apurv Rathore <thelegend2710@gmail.com>
2022-01-14 23:12:51 +0530

    Add solution for 1472C

---
+++ 1472/C.py
@@ -0,0 +1,16 @@
+from math import ceil,log
+from collections import defaultdict
+from sys import stdin
+# l = list(map(int,stdin.readline().split()))
+t = int(input())
+for _ in range(t):
+    n = int(input())
+    l = list(map(int,input().split()))
+    suf = l[:]
+    
+    for i in range(n-1,-1,-1):
+        if (i+l[i]<n):
+            suf[i]+=suf[i+l[i]]
+    print(max(suf))
+    
+    
