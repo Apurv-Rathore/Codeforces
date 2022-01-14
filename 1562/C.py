23702a3200dc6af9d6dbe1a222c437b369d61017
Apurv Rathore <thelegend2710@gmail.com>
2022-01-14 22:34:00 +0530

    Add solution for 1562C

---
+++ 1562/C.py
@@ -0,0 +1,20 @@
+def ffff(s,x,n):
+    print(x+1,n,x+2,n)
+def fff(n):
+    print(2,n//2+1,1,n//2)
+def ff(s,n):
+    for i in range(n//2,n):
+        if s[i]=='0':
+            print(1,i+1,1,i)
+            return True 
+    return False 
+for _ in range(int(input())):
+    n = int(input())
+    s = list(input())   
+    if ff(s,n):
+        continue 
+    if '0' not in s:
+        fff(n)
+    else:
+        x = s.index('0')
+        ffff(s,x,n)
