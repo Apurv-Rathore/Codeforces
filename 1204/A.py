2cccc400532fd96adb67df30b53234b1291cfed4
Apurv Rathore <thelegend2710@gmail.com>
2022-01-14 23:04:21 +0530

    Add solution for 1204A

---
+++ 1204/A.py
@@ -0,0 +1,17 @@
+import sys,os,io
+if(os.path.exists('input.txt')):
+    sys.stdin = open("input.txt","r") ; sys.stdout = open("output.txt","w") 
+# else:
+#     input = io.BytesIO(os.read(0, os.fstat(0).st_size)).readline
+t = 1
+# t = int(input())
+for _ in range(t):
+    # n = int(input())
+    # l = [int(x) for x in input().split()]
+    s = input()
+    y = len(s)-1
+    ans = y//2
+    if y%2==1 or s.count('1')>1:
+        ans+=1
+    print(ans)
+
